772dc629315a114149064bd642672ea04b3a21f5
Phong Phan <haiphong0912@gmail.com>
2024-09-27 07:47:54 -0400

    change apple dev information to myself

--- xcode13/Shared (App)/ViewController.swift
+++ xcode13/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "jp.co.birchill.tenten-ja-reader.Extension"
+let extensionBundleIdentifier = "com.phongp.wenzi.Extension"
 
 #if os(iOS)
 struct Device {
