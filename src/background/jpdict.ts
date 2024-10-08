e0809d248da069fe17cbf69751aa7506a6e65c70
Phong Phan <haiphong0912@gmail.com>
2024-10-08 11:51:58 -0400

    disable idb dict downloading, updating

--- src/background/jpdict.ts
+++ src/background/jpdict.ts
@@ -61,40 +61,40 @@ export type JpdictStateWithFallback = Omit<JpdictState, 'words'> & {
 // Minimum amount of time to wait before checking for database updates.
 //
 
-const UPDATE_THRESHOLD_MS = 12 * 60 * 60 * 1000; // 12 hours
+// const UPDATE_THRESHOLD_MS = 12 * 60 * 60 * 1000; // 12 hours
 
 //
 // Backend setup
 //
 
-const backend: JpdictBackend =
-  'Worker' in self ? new JpdictWorkerBackend() : new JpdictLocalBackend();
+// const backend: JpdictBackend =
+//   'Worker' in self ? new JpdictWorkerBackend() : new JpdictLocalBackend();
 
 // Local state tracking
 //
 // We track some state locally because we want to avoid querying the database
 // when it is being updated since this can block for several seconds.
 
-let dbState: JpdictStateWithFallback = {
-  words: {
-    state: 'init',
-    version: null,
-    fallbackState: 'unloaded',
-  },
-  kanji: {
-    state: 'init',
-    version: null,
-  },
-  radicals: {
-    state: 'init',
-    version: null,
-  },
-  names: {
-    state: 'init',
-    version: null,
-  },
-  updateState: { type: 'idle', lastCheck: null },
-};
+// let dbState: JpdictStateWithFallback = {
+//   words: {
+//     state: 'init',
+//     version: null,
+//     fallbackState: 'unloaded',
+//   },
+//   kanji: {
+//     state: 'init',
+//     version: null,
+//   },
+//   radicals: {
+//     state: 'init',
+//     version: null,
+//   },
+//   names: {
+//     state: 'init',
+//     version: null,
+//   },
+//   updateState: { type: 'idle', lastCheck: null },
+// };
 
 // Is the IDB database available for the given series?
 //
@@ -142,14 +142,14 @@ const fallbackDatabaseLoader = new FlatFileDatabaseLoader({
 // because the API is async, on Chrome we can get situations where we actually
 // end up applying the database state messages in the wrong order.
 
-let lastUpdateTime: number | null = null;
+// let lastUpdateTime: number | null = null;
 
-//
-// Public API
-//
+// //
+// // Public API
+// //
 
-let initPromise: Promise<void> | undefined;
-let initComplete = false;
+// let initPromise: Promise<void> | undefined;
+// let initComplete = false;
 
 export async function initDb({
   lang,
@@ -158,153 +158,155 @@ export async function initDb({
   lang: string;
   onUpdate: (status: JpdictStateWithFallback) => void;
 }) {
-  if (initPromise) {
-    await initPromise;
-    return;
-  }
+  // if (initPromise) {
+  //   await initPromise;
+  //   return;
+  // }
 
-  let resolveInitPromise: () => void;
-  initPromise = new Promise((resolve) => {
-    resolveInitPromise = resolve;
-  });
+  // let resolveInitPromise: () => void;
+  // initPromise = new Promise((resolve) => {
+  //   resolveInitPromise = resolve;
+  // });
 
-  lastUpdateTime = await getLastUpdateTime();
-  Bugsnag.leaveBreadcrumb(`Got last update time of ${lastUpdateTime}`);
+  // lastUpdateTime = await getLastUpdateTime();
+  // Bugsnag.leaveBreadcrumb(`Got last update time of ${lastUpdateTime}`);
 
-  // Register the listener
-  backend.addEventListener(async (event: JpdictEvent) => {
-    switch (event.type) {
-      case 'dbstateupdated':
-        {
-          // Prepare the new state while preserving the existing fallback state.
-          const state = {
-            ...event.state,
-            words: {
-              ...event.state.words,
-              fallbackState: dbState.words.fallbackState,
-            },
-          };
+  // // Register the listener
+  // backend.addEventListener(async (event: JpdictEvent) => {
+  //   switch (event.type) {
+  //     case 'dbstateupdated':
+  //       {
+  //         // Prepare the new state while preserving the existing fallback state.
+  //         const state = {
+  //           ...event.state,
+  //           words: {
+  //             ...event.state.words,
+  //             fallbackState: dbState.words.fallbackState,
+  //           },
+  //         };
 
-          // Fill out the lastCheck field in the updateState.
-          //
-          // This value will only be set if we already did a check this session.
-          // It is _not_ a stored value.  So, if it is not set, use the value we
-          // stored instead.
-          if (state.updateState.lastCheck === null && lastUpdateTime) {
-            state.updateState.lastCheck = new Date(lastUpdateTime);
-          }
+  //         // Fill out the lastCheck field in the updateState.
+  //         //
+  //         // This value will only be set if we already did a check this session.
+  //         // It is _not_ a stored value.  So, if it is not set, use the value we
+  //         // stored instead.
+  //         if (state.updateState.lastCheck === null && lastUpdateTime) {
+  //           state.updateState.lastCheck = new Date(lastUpdateTime);
+  //         }
 
-          dbState = state;
+  //         dbState = state;
 
-          try {
-            onUpdate(state);
-          } catch (e) {
-            void Bugsnag.notify(e);
-          }
+  //         try {
+  //           onUpdate(state);
+  //         } catch (e) {
+  //           void Bugsnag.notify(e);
+  //         }
 
-          if (!initComplete) {
-            initComplete = true;
-            resolveInitPromise();
-          }
-        }
-        break;
+  //         if (!initComplete) {
+  //           initComplete = true;
+  //           resolveInitPromise();
+  //         }
+  //       }
+  //       break;
 
-      case 'dbupdatecomplete':
-        if (event.lastCheck) {
-          void setLastUpdateTime(event.lastCheck.getTime());
-        }
-        break;
+  //     case 'dbupdatecomplete':
+  //       if (event.lastCheck) {
+  //         void setLastUpdateTime(event.lastCheck.getTime());
+  //       }
+  //       break;
 
-      case 'breadcrumb':
-        Bugsnag.leaveBreadcrumb(event.message);
-        break;
+  //     case 'breadcrumb':
+  //       Bugsnag.leaveBreadcrumb(event.message);
+  //       break;
 
-      case 'error':
-        {
-          const error = new Error(event.message);
-          error.name = event.name;
-          error.stack = event.stack;
-          void Bugsnag.notify(error, { severity: event.severity });
-        }
-        break;
-    }
-  });
+  //     case 'error':
+  //       {
+  //         const error = new Error(event.message);
+  //         error.name = event.name;
+  //         error.stack = event.stack;
+  //         void Bugsnag.notify(error, { severity: event.severity });
+  //       }
+  //       break;
+  //   }
+  // });
 
-  // Make sure updates to the fallback database loading state are also reported.
-  //
-  // But first, reset any loads that might have errored or hung so that the
-  // user can retry the load by disabling/enabling the add-on.
+  //   // Make sure updates to the fallback database loading state are also reported.
+  //   //
+  //   // But first, reset any loads that might have errored or hung so that the
+  //   // user can retry the load by disabling/enabling the add-on.
   fallbackDatabaseLoader.resetIfNotLoaded();
-  fallbackDatabaseLoader.onUpdate = (
-    fallbackDatabaseState: FlatFileDatabaseLoadState
-  ) => {
-    dbState.words.fallbackState = fallbackDatabaseState;
-    onUpdate(dbState);
-  };
+  // load flat file database right at the beginning
+  await fallbackDatabaseLoader.database;
+  // fallbackDatabaseLoader.onUpdate = (
+  //   fallbackDatabaseState: FlatFileDatabaseLoadState
+  // ) => {
+  //   dbState.words.fallbackState = fallbackDatabaseState;
+  //   onUpdate(dbState);
+  // };
 
-  // Fetch the initial state
-  backend.queryState();
+  //   // Fetch the initial state
+  //   backend.queryState();
 
-  // If we updated within the minimum window then we don't need to update
-  if (lastUpdateTime && Date.now() - lastUpdateTime < UPDATE_THRESHOLD_MS) {
-    Bugsnag.leaveBreadcrumb('Downloaded data is up-to-date');
-  } else {
-    updateDb({ lang, force: false });
-  }
+  //   // If we updated within the minimum window then we don't need to update
+  //   if (lastUpdateTime && Date.now() - lastUpdateTime < UPDATE_THRESHOLD_MS) {
+  //     Bugsnag.leaveBreadcrumb('Downloaded data is up-to-date');
+  //   } else {
+  //     updateDb({ lang, force: false });
+  //   }
 
-  await initPromise;
+  //   await initPromise;
 }
 
-async function getLastUpdateTime(): Promise<number | null> {
-  try {
-    const getResult = await browser.storage.local.get('lastDbUpdateTime');
-    if (typeof getResult.lastDbUpdateTime === 'number') {
-      return getResult.lastDbUpdateTime as number;
-    }
-  } catch {
-    // Extension storage can sometimes randomly fail with 'An unexpected error
-    // occurred'. Ignore, but log it.
-    console.warn('Failed to get last update time from storage');
-  }
+// async function getLastUpdateTime(): Promise<number | null> {
+//   try {
+//     const getResult = await browser.storage.local.get('lastDbUpdateTime');
+//     if (typeof getResult.lastDbUpdateTime === 'number') {
+//       return getResult.lastDbUpdateTime as number;
+//     }
+//   } catch {
+//     // Extension storage can sometimes randomly fail with 'An unexpected error
+//     // occurred'. Ignore, but log it.
+//     console.warn('Failed to get last update time from storage');
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
-async function setLastUpdateTime(time: number | null) {
-  // Make sure to update the local version too.
-  lastUpdateTime = time;
+// async function setLastUpdateTime(time: number | null) {
+//   // Make sure to update the local version too.
+//   lastUpdateTime = time;
 
-  // Extension storage can randomly fail with "An unexpected error occurred".
-  try {
-    if (time) {
-      await browser.storage.local.set({
-        lastDbUpdateTime: time,
-      });
-    } else {
-      await browser.storage.local.remove('lastDbUpdateTime');
-    }
+//   // Extension storage can randomly fail with "An unexpected error occurred".
+//   try {
+//     if (time) {
+//       await browser.storage.local.set({
+//         lastDbUpdateTime: time,
+//       });
+//     } else {
+//       await browser.storage.local.remove('lastDbUpdateTime');
+//     }
 
-    // Try to remove any old value we stored so we don't end up using it
-    // accidentally.
-    browser.storage.local.remove('lastUpdateKanjiDb').catch(() => {
-      /* Ignore */
-    });
-  } catch {
-    // Don't notify Bugsnag because this is a common error in Firefox.
-  }
-}
+//     // Try to remove any old value we stored so we don't end up using it
+//     // accidentally.
+//     browser.storage.local.remove('lastUpdateKanjiDb').catch(() => {
+//       /* Ignore */
+//     });
+//   } catch {
+//     // Don't notify Bugsnag because this is a common error in Firefox.
+//   }
+// }
 
 export function updateDb(params: { lang: string; force: boolean }) {
-  backend.updateDb(params);
+  // backend.updateDb(params);
 }
 
 export function cancelUpdateDb() {
-  backend.cancelUpdateDb();
+  // backend.cancelUpdateDb();
 }
 
 export function deleteDb() {
-  backend.deleteDb();
-  void setLastUpdateTime(null);
+  // backend.deleteDb();
+  // void setLastUpdateTime(null);
 }
 
 // ---------------------------------------------------------------------------
@@ -382,47 +384,48 @@ export async function translate({
   text: string;
   includeRomaji?: boolean;
 }): Promise<TranslateResult | null> {
-  const result: TranslateResult = {
-    type: 'translate',
-    data: [],
-    textLen: text.length,
-    more: false,
-  };
+  return null;
+  // const result: TranslateResult = {
+  //   type: 'translate',
+  //   data: [],
+  //   textLen: text.length,
+  //   more: false,
+  // };
 
-  let skip: number;
-  while (text.length > 0) {
-    const [searchResult, dbStatus] = await searchWords({
-      input: text,
-      max: 1,
-      includeRomaji,
-    });
+  // let skip: number;
+  // while (text.length > 0) {
+  //   const [searchResult, dbStatus] = await searchWords({
+  //     input: text,
+  //     max: 1,
+  //     includeRomaji,
+  //   });
 
-    if (searchResult && searchResult.data) {
-      if (result.data.length >= WORDS_MAX_ENTRIES) {
-        result.more = true;
-        break;
-      }
+  //   if (searchResult && searchResult.data) {
+  //     if (result.data.length >= WORDS_MAX_ENTRIES) {
+  //       result.more = true;
+  //       break;
+  //     }
 
-      // Just take first match
-      result.data.push(searchResult.data[0]);
-      skip = searchResult.matchLen;
-    } else {
-      skip = 1;
-    }
+  //     // Just take first match
+  //     result.data.push(searchResult.data[0]);
+  //     skip = searchResult.matchLen;
+  //   } else {
+  //     skip = 1;
+  //   }
 
-    if (searchResult && dbStatus) {
-      result.dbStatus = dbStatus;
-    }
+  //   if (searchResult && dbStatus) {
+  //     result.dbStatus = dbStatus;
+  //   }
 
-    text = text.substring(skip);
-  }
+  //   text = text.substring(skip);
+  // }
 
-  if (result.data.length === 0) {
-    return null;
-  }
+  // if (result.data.length === 0) {
+  //   return null;
+  // }
 
-  result.textLen -= text.length;
-  return result;
+  // result.textLen -= text.length;
+  // return result;
 }
 
 // ---------------------------------------------------------------------------
@@ -434,83 +437,84 @@ export async function translate({
 export async function searchKanji(
   input: string
 ): Promise<KanjiSearchResult | null | 'unavailable' | 'updating'> {
-  const kanjiStatus = getDataSeriesStatus('kanji');
-  const radicalStatus = getDataSeriesStatus('radicals');
-  if (kanjiStatus === 'unavailable' || radicalStatus === 'unavailable') {
-    return 'unavailable';
-  }
+  return null;
+  // const kanjiStatus = getDataSeriesStatus('kanji');
+  // const radicalStatus = getDataSeriesStatus('radicals');
+  // if (kanjiStatus === 'unavailable' || radicalStatus === 'unavailable') {
+  //   return 'unavailable';
+  // }
 
-  if (kanjiStatus === 'updating' || radicalStatus === 'updating') {
-    return 'updating';
-  }
+  // if (kanjiStatus === 'updating' || radicalStatus === 'updating') {
+  //   return 'updating';
+  // }
 
-  // Normalize the input in order to be able to parse radicals as kanji.
-  const [normalized] = normalizeInput(input);
+  // // Normalize the input in order to be able to parse radicals as kanji.
+  // const [normalized] = normalizeInput(input);
 
-  // Do some very elementary filtering on kanji
-  //
-  // We know that the input should be mostly Japanese so we just do some very
-  // basic filtering to drop any hiragana / katakana.
-  //
-  // We _could_ do a more thoroughgoing check based on all the different Unicode
-  // ranges but they're constantly being expanded and if some obscure character
-  // ends up in the kanji database we want to show it even if it doesn't match
-  // our expectations of what characters are kanji.
-  const kanjiLastIndex = new Map<string, number>();
-  const kanji = [
-    ...new Set(
-      [...normalized].filter((c, i) => {
-        const cp = c.codePointAt(0)!;
-        const isKanji =
-          // Don't bother looking up Latin text
-          cp >= 0x3000 &&
-          // Or hiragana (yeah, 0x1b0001 is also hiragana but this is good enough)
-          !(cp >= 0x3040 && cp <= 0x309f) &&
-          // Or katakana
-          !(cp >= 0x30a0 && cp <= 0x30ff) &&
-          !(cp >= 0x31f0 && cp <= 0x31ff) &&
-          // Or half-width katakana
-          !(cp >= 0xff65 && cp <= 0xff9f);
-        if (isKanji) {
-          kanjiLastIndex.set(c, i);
-        }
+  // // Do some very elementary filtering on kanji
+  // //
+  // // We know that the input should be mostly Japanese so we just do some very
+  // // basic filtering to drop any hiragana / katakana.
+  // //
+  // // We _could_ do a more thoroughgoing check based on all the different Unicode
+  // // ranges but they're constantly being expanded and if some obscure character
+  // // ends up in the kanji database we want to show it even if it doesn't match
+  // // our expectations of what characters are kanji.
+  // const kanjiLastIndex = new Map<string, number>();
+  // const kanji = [
+  //   ...new Set(
+  //     [...normalized].filter((c, i) => {
+  //       const cp = c.codePointAt(0)!;
+  //       const isKanji =
+  //         // Don't bother looking up Latin text
+  //         cp >= 0x3000 &&
+  //         // Or hiragana (yeah, 0x1b0001 is also hiragana but this is good enough)
+  //         !(cp >= 0x3040 && cp <= 0x309f) &&
+  //         // Or katakana
+  //         !(cp >= 0x30a0 && cp <= 0x30ff) &&
+  //         !(cp >= 0x31f0 && cp <= 0x31ff) &&
+  //         // Or half-width katakana
+  //         !(cp >= 0xff65 && cp <= 0xff9f);
+  //       if (isKanji) {
+  //         kanjiLastIndex.set(c, i);
+  //       }
 
-        return isKanji;
-      })
-    ),
-  ];
+  //       return isKanji;
+  //     })
+  //   ),
+  // ];
 
-  const logWarningMessage = (message: string) => {
-    // Ignore certain warnings that are not currently meaningful
-    if (message.startsWith("Couldn't find a radical or kanji entry for")) {
-      return;
-    }
+  // const logWarningMessage = (message: string) => {
+  //   // Ignore certain warnings that are not currently meaningful
+  //   if (message.startsWith("Couldn't find a radical or kanji entry for")) {
+  //     return;
+  //   }
 
-    void Bugsnag.notify(message, { severity: 'warning' });
-  };
+  //   void Bugsnag.notify(message, { severity: 'warning' });
+  // };
 
-  let result;
-  try {
-    result = await getKanji({
-      kanji,
-      lang: dbState.kanji.version?.lang ?? 'en',
-      logWarningMessage,
-    });
-  } catch (e) {
-    console.error('Error looking up kanji', e);
-    void Bugsnag.notify(e || '(Error looking up kanji)');
-    return null;
-  }
+  // let result;
+  // try {
+  //   result = await getKanji({
+  //     kanji,
+  //     lang: dbState.kanji.version?.lang ?? 'en',
+  //     logWarningMessage,
+  //   });
+  // } catch (e) {
+  //   console.error('Error looking up kanji', e);
+  //   void Bugsnag.notify(e || '(Error looking up kanji)');
+  //   return null;
+  // }
 
-  if (!result.length) {
-    return null;
-  }
+  // if (!result.length) {
+  //   return null;
+  // }
 
-  // Work out what the last matched character was
-  const matchLen =
-    Math.max(...result.map((r) => kanjiLastIndex.get(r.c) || 0)) + 1;
+  // // Work out what the last matched character was
+  // const matchLen =
+  //   Math.max(...result.map((r) => kanjiLastIndex.get(r.c) || 0)) + 1;
 
-  return { type: 'kanji', data: result, matchLen };
+  // return { type: 'kanji', data: result, matchLen };
 }
 
 // ---------------------------------------------------------------------------
@@ -519,7 +523,7 @@ export async function searchKanji(
 //
 // ---------------------------------------------------------------------------
 
-const NAMES_MAX_ENTRIES = 20;
+// const NAMES_MAX_ENTRIES = 20;
 
 export async function searchNames({
   abortSignal,
@@ -530,18 +534,19 @@ export async function searchNames({
   input: string;
   minLength?: number;
 }): Promise<NameSearchResult | null | 'unavailable' | 'updating'> {
-  const dbStatus = getDataSeriesStatus('names');
-  if (dbStatus !== 'ok') {
-    return dbStatus;
-  }
+  return null;
+  // const dbStatus = getDataSeriesStatus('names');
+  // if (dbStatus !== 'ok') {
+  //   return dbStatus;
+  // }
 
-  const [normalized, inputLengths] = normalizeInput(input);
+  // const [normalized, inputLengths] = normalizeInput(input);
 
-  return nameSearch({
-    abortSignal,
-    input: normalized,
-    inputLengths,
-    minInputLength: minLength,
-    maxResults: NAMES_MAX_ENTRIES,
-  });
+  // return nameSearch({
+  //   abortSignal,
+  //   input: normalized,
+  //   inputLengths,
+  //   minInputLength: minLength,
+  //   maxResults: NAMES_MAX_ENTRIES,
+  // });
 }
